ccde293c153ba88818b98de7c2cec3b2edc2992c
Zeynep Aslı Acar <156081425+ZeynepAsliAcar@users.noreply.github.com>
2025-07-20 14:18:53 +0300

    FunctionPalindrome.py

--- FunctionPalindrome.py
+++ FunctionPalindrome.py
@@ -1,15 +1,22 @@
-def palindrome(S):
-    OPT = new int[S.length][S.length + 1]
- 
-    for i = 0 to S.length - 1:
-        for n = 0 to 1:
-            OPT[i][n] = n
-    
-    for n = 2 to S.length:
-        for i = 0 to S.length - n:
-            if S[i] == S[i + n - 1]  and  OPT[i+1][n-2] == n-2:
-                OPT[i][n] = 2 + OPT[i+1][n-2]
+def longest_palindrome_subseq(S):
+    n = len(S)
+    OPT = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        OPT[i][i] = 1
+
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if S[i] == S[j]:
+                if length == 2:
+                    OPT[i][j] = 2
+                else:
+                    OPT[i][j] = 2 + OPT[i + 1][j - 1]
             else:
-                OPT[i][n] = max(OPT[i][n-1], OPT[i+1][n-1])
-    
-    return OPT[0][S.length]
+                OPT[i][j] = max(OPT[i + 1][j], OPT[i][j - 1])
+
+    return OPT[0][n - 1]
+
+S = input("Bir string girin: ")
+print("En uzun palindromik alt dizinin uzunluğu:", longest_palindrome_subseq(S))
